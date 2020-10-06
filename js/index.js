0eea1e6441bcce59dc07b88b138008cefe7ab429
Colin Faivre <colin.faivre@gmail.com>
2020-10-06 13:13:41 +0200

    Add recursion

--- js/index.js
+++ js/index.js
@@ -148,10 +148,64 @@ import HashTable from './dataStructures/HashTable.js';
 
 // HASH TABLE
 
-const hash = new HashTable();
-hash.put('Gandalf', 'gandalf@email.com');
-hash.put('John', 'johnsnow@email.com');
-hash.put('Tyrion', 'tyrion@email.com');
+// const hash = new HashTable();
+// hash.put('Gandalf', 'gandalf@email.com');
+// hash.put('John', 'johnsnow@email.com');
+// hash.put('Tyrion', 'tyrion@email.com');
 
-console.log(hash.hashCode('Gandalf') + ' - Gandalf');
-console.log(hash.get('John'));
+// console.log(hash.hashCode('Gandalf') + ' - Gandalf');
+// console.log(hash.get('John'));
+
+
+
+// RECURSION
+
+function factorialIterative(number) {
+    if (number < 0) return undefined;
+
+    let total = 1;
+    for (let n = number; n > 1; n--) {
+        total *= n;
+    }
+
+    return total;
+}
+
+console.log(factorialIterative(10));
+
+function factorial(n) {
+    if (n === 1 || n === 0) {
+        return 1;
+    }
+
+    return n * factorial(n - 1)
+}
+console.log(factorial(9));
+
+function fibonacciIterative(n) {
+    if (n < 1) return 0;
+    if (n <= 2) return 1;
+
+    let fibNMinus2 = 0;
+    let fibNMinus1 = 1;
+    let fibN = n;
+
+    for (let i = 2; i <= n; i++) {
+        fibN = fibNMinus1 + fibNMinus2;
+        fibNMinus2 = fibNMinus1;
+        fibNMinus1 = fibN;
+    }
+
+    return fibN;
+}
+
+console.log(fibonacciIterative(9));
+
+function fibonacci(n) {
+    if (n < 1) return 0;
+    if (n <= 2) return 1;
+
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+console.log(fibonacci(10));
